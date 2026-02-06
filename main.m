ca0116938ce9380307a01028680a64ec5dc0e543
shinyatlas <mannyoso07@gmail.com>
2026-02-06 17:31:31 -0500

    added a struct to hold model parameters

--- main.m
+++ main.m
@@ -5,3 +5,8 @@ full_path = fullfile(pathname, filename);
 %load data into matlab 
 readtable(full_path)
 
+%define a structure to hold all parameters
+params.rI = 0.05; %immune cell growth rate
+params.rC = 0.08; %cancer cell growth rate
+% add other parameters here when they are known
+
