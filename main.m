7b7a52eddc80512c7621ee61c41c789b25394a14
shinyatlas <mannyoso07@gmail.com>
2026-02-06 19:13:03 -0500

    continued adding parameters

--- main.m
+++ main.m
@@ -5,8 +5,19 @@ full_path = fullfile(pathname, filename);
 %load data into matlab 
 readtable(full_path)
 
-%define a structure to hold all parameters
-params.rI = 0.05; %immune cell growth rate
-params.rC = 0.08; %cancer cell growth rate
-% add other parameters here when they are known
+%define a structure to hold all model parameters
+params.rI = float; %immune cell growth rate
+params.rC = float; %cancer cell growth rate
+params.dI = float; %immune cell death rate
+params.dC = float; %cancer cell death rate
+params.betaI = float; %cytokine production rate per immune cell
+params.betaC = float; %cytokine production rate per cancer cell
+params.kIC = float; %strength of immune killing/suppression of cancer cells
+params.dY = float; %cytokine decay/clearance rate
+params.alphaI = float; %effect of cytokine Y on immune activation or growth
+params.D = int; %dimensionless drug level (0 for no drug; % 1 for full-dose culture condition)
+%missing parameters : "drug" parameters
+
+
+
 
