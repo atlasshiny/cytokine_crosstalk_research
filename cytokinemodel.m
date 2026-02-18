94ae33941ec156ee365fb20e8fc3d485cbe62509
shinyatlas <mannyoso07@gmail.com>
2026-02-17 22:58:34 -0500

    Added all major rate of change equations for immune/cancer cells and cytokine concentration

--- cytokinemodel.m
+++ cytokinemodel.m
@@ -1,15 +1,26 @@
-function dydt = cytokinemodel(t,y,p)
+function dydt = cytokinemodel(~,y,p)
 %Comprehensive model for cytokine modeling
-%   Detailed explanation goes here
+%   Calculates the changes in the immune cells, cancer cells, and the
+%   general cytokine concentration with respect to time.
 arguments (Input)
-    t (1,1) double %time (must be a scalar)
-    y (3,1) double %current cell count at a given t-value
+    ~ %time (must be a scalar; if incorporating time: t (1,1) double)
+    y (3,1) double %current cell/cytokine count at a given t-value (a column vector). If starting at t=0, this is the starting amount of cells/cytokines.
     p CytokineParameter %an instance of the CytokineParameter class
 end
 
 arguments (Output)
-    dydt (3,1)
+    dydt (3,1) %a column vector containing the rates of change 
 end
+%pull components of the vector y
+I=y(1);
+C=y(2);
+Y=y(3);
+
+%calculate the rate of change in immune cells with respect to time
+dIdt = p.rI * (1 - p.deltaI * p.D) * I + p.alphaI * Y * I - p.dI * I; %change in immune cells with respect to time
+dCdt = p.rC * (1 - p.deltaC * p.D) * C - p.dC * C - p.kIC * I * C; %change in cancer cells with respect to time
+dYdt = p.betaI * (1 - p.epsilonI * p.D) * I + p.betaC * (1 - p.epsilonC * p.D) * C - p.dY * Y; %change in cytokine concentration with respect to time
+
 %the return vector with all of the calculated rate of changes
-dydt = [];
+dydt = [dIdt;dCdt;dYdt];
 end
