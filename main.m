f2b7b2918c6bdee9844718973dae44de87dd23b6
shinyatlas <mannyoso07@gmail.com>
2026-02-23 20:42:56 -0500

    commented out the adding data file process since there are currently no data files

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 %select the data file using the standard windows file viewer
-[filename, pathname] = uigetfile('*.csv', 'Select dataset');
-full_path = fullfile(pathname, filename);
+%[filename, pathname] = uigetfile('*.csv', 'Select dataset');
+%full_path = fullfile(pathname, filename);
 
 %load data into matlab 
-data_set = readtable(full_path);
+%data_set = readtable(full_path);
 
 params = CytokineParameter("rC",0.05,"dC",0.01);
 
